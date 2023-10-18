dadd2f821d86e11d10d04b94447789d85164a547
John-Development <38006800+John-Development@users.noreply.github.com>
2023-10-18 13:44:30 +0200

    Improve PistonHandlerMixin to avoid Overwrite

--- src/main/java/net/redstoneOverpower/mixin/block/piston/PistonHandlerMixin.java
+++ src/main/java/net/redstoneOverpower/mixin/block/piston/PistonHandlerMixin.java
@@ -2,8 +2,6 @@ package net.redstoneOverpower.mixin.block.piston;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.PistonBlock;
-import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.block.piston.PistonHandler;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
@@ -12,31 +10,24 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Mixin(PistonHandler.class)
 public abstract class PistonHandlerMixin {
   @Shadow @Final
   private World world;
   @Shadow @Final
-  private BlockPos posFrom;
-  @Shadow @Final
   private Direction motionDirection;
-  @Final @Shadow
-  private final List<BlockPos> movedBlocks = new ArrayList<>();
-  @Final @Shadow
-  private final List<BlockPos> brokenBlocks = new ArrayList<>();
 
   @Shadow
-  private static boolean isBlockSticky(BlockState state) {
+  private boolean tryMove(BlockPos blockPos, Direction direction) {
     return false;
   }
-
   @Shadow
-  protected abstract void setMovedBlocks(int from, int to);
+  private static boolean isAdjacentBlockStuck(BlockState state, BlockState adjacentState) {
+    return false;
+  }
 
   @Inject(method = "isBlockSticky", at = @At(value = "HEAD"), cancellable = true)
   private static void isBlockStickyMixin(BlockState state, CallbackInfoReturnable<Boolean> cir) {
@@ -70,13 +61,8 @@ public abstract class PistonHandlerMixin {
     ) {
       return false;
     }
-    if (state.isOf(Blocks.HONEY_BLOCK) && adjacentState.isOf(Blocks.SLIME_BLOCK)) {
-      return false;
-    }
-    if (state.isOf(Blocks.SLIME_BLOCK) && adjacentState.isOf(Blocks.HONEY_BLOCK)) {
-      return false;
-    }
-    return PistonHandlerMixin.isBlockSticky(state) || PistonHandlerMixin.isBlockSticky(adjacentState);
+
+    return isAdjacentBlockStuck(state, adjacentState);
   }
 
    /**
@@ -92,88 +78,22 @@ public abstract class PistonHandlerMixin {
         || !isAdjacentBlockStuck(blockState, this.world.getBlockState(blockPos = pos.offset(direction)), direction)
         || this.tryMove(blockPos, direction)
       ) continue;
+
       return false;
     }
+
     return true;
   }
 
-  /**
-   * @author Juarrin
-   * @reason different isAdjacentBlockStuck invocation
-   */
-  @Overwrite
-  private boolean tryMove(BlockPos pos, Direction dir) {
-    int k;
-    BlockState blockState = this.world.getBlockState(pos);
-    if (blockState.isAir()) {
-      return true;
-    }
-    if (!PistonBlock.isMovable(blockState, this.world, pos, this.motionDirection, false, dir)) {
-      return true;
-    }
-    if (pos.equals(this.posFrom)) {
-      return true;
-    }
-    if (this.movedBlocks.contains(pos)) {
-      return true;
-    }
-    int i = 1;
-    if (i + this.movedBlocks.size() > 12) {
-      return false;
-    }
-    while (PistonHandlerMixin.isBlockSticky(blockState)) {
-      BlockPos blockPos = pos.offset(this.motionDirection.getOpposite(), i);
-      BlockState blockState2 = blockState;
-      blockState = this.world.getBlockState(blockPos);
-      if (blockState.isAir()
-        || !isAdjacentBlockStuck(blockState2, blockState, this.motionDirection)
-        || !PistonBlock.isMovable(blockState, this.world, blockPos, this.motionDirection, false, this.motionDirection.getOpposite())
-        || blockPos.equals(this.posFrom)
-      ) {
-        break;
-      }
-      if (++i + this.movedBlocks.size() <= 12) {
-        continue;
-      }
-      return false;
-    }
-    int j = 0;
-    for (k = i - 1; k >= 0; --k) {
-      this.movedBlocks.add(pos.offset(this.motionDirection.getOpposite(), k));
-      ++j;
-    }
-    k = 1;
-    while (true) {
-      BlockPos blockPos2;
-      int l;
-      if ((l = this.movedBlocks.indexOf(blockPos2 = pos.offset(this.motionDirection, k))) > -1) {
-        this.setMovedBlocks(j, l);
-        for (int m = 0; m <= l + j; ++m) {
-          BlockPos blockPos3 = this.movedBlocks.get(m);
-          if (!PistonHandlerMixin.isBlockSticky(this.world.getBlockState(blockPos3)) || this.tryMoveAdjacentBlock(blockPos3)) continue;
-          return false;
-        }
-        return true;
-      }
-      blockState = this.world.getBlockState(blockPos2);
-      if (blockState.isAir()) {
-        return true;
-      }
-      if (!PistonBlock.isMovable(blockState, this.world, blockPos2, this.motionDirection, true, this.motionDirection)
-        || blockPos2.equals(this.posFrom)
-      ) {
-        return false;
-      }
-      if (blockState.getPistonBehavior() == PistonBehavior.DESTROY) {
-        this.brokenBlocks.add(blockPos2);
-        return true;
-      }
-      if (this.movedBlocks.size() >= 12) {
-        return false;
-      }
-      this.movedBlocks.add(blockPos2);
-      ++j;
-      ++k;
-    }
+  @Redirect(
+    method = "tryMove",
+    at = @At(
+      value = "INVOKE",
+      target = "Lnet/minecraft/block/piston/PistonHandler;isAdjacentBlockStuck(Lnet/minecraft/block/BlockState;Lnet/minecraft/block/BlockState;)Z",
+      remap = false
+    )
+  )
+  private boolean isAdjacentBlockStuckRedirect(BlockState state, BlockState adjacentState, BlockPos pos, Direction dir) {
+    return isAdjacentBlockStuck(state, adjacentState, this.motionDirection);
   }
 }
