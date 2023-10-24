6abd1ec28972930e7ded82a9c474b8cf9270f594
John-Development <38006800+John-Development@users.noreply.github.com>
2023-10-24 20:26:41 +0200

    Added logical comparator block

--- src/main/java/net/redstoneOverpower/RedstoneOverpower.java
+++ src/main/java/net/redstoneOverpower/RedstoneOverpower.java
@@ -2,14 +2,30 @@ package net.redstoneOverpower;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.redstoneOverpower.block.LogicalComparatorBlock;
+import net.redstoneOverpower.block.entity.LogicalComparatorBlockEntity;
 
 public class RedstoneOverpower implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("redstone-overpower");
+
+	public static final LogicalComparatorBlock LOGICAL_COMPARATOR = new LogicalComparatorBlock(FabricBlockSettings.copyOf(Blocks.COMPARATOR));
+
+	public static final BlockEntityType<LogicalComparatorBlockEntity> LOGICAL_COMPARATOR_ENTITY = Registry.register(
+		Registries.BLOCK_ENTITY_TYPE,
+		new Identifier("redstoneoverpower", "logical_comparator_block_entity"),
+		FabricBlockEntityTypeBuilder.create(LogicalComparatorBlockEntity::new, LOGICAL_COMPARATOR).build()
+	);
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +33,7 @@ public class RedstoneOverpower implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("This is a new mod!");
+		Registry.register(Registries.BLOCK, new Identifier("redstoneoverpower", "logical_comparator"), LOGICAL_COMPARATOR);
+		Registry.register(Registries.ITEM, new Identifier("redstoneoverpower", "logical_comparator"), new BlockItem(LOGICAL_COMPARATOR, new FabricItemSettings()));
 	}
 }
---
+++ src/main/java/net/redstoneOverpower/block/entity/LogicalComparatorBlockEntity.java
@@ -0,0 +1,34 @@
+package net.redstoneOverpower.block.entity;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
+
+import static net.redstoneOverpower.RedstoneOverpower.LOGICAL_COMPARATOR_ENTITY;
+
+public class LogicalComparatorBlockEntity extends BlockEntity {
+    private int outputSignal;
+
+    public LogicalComparatorBlockEntity(BlockPos pos, BlockState state) {
+        super(LOGICAL_COMPARATOR_ENTITY, pos, state);
+    }
+
+    protected void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        nbt.putInt("OutputSignal", this.outputSignal);
+    }
+
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        this.outputSignal = nbt.getInt("OutputSignal");
+    }
+
+    public int getOutputSignal() {
+        return this.outputSignal;
+    }
+
+    public void setOutputSignal(int outputSignal) {
+        this.outputSignal = outputSignal;
+    }
+}
